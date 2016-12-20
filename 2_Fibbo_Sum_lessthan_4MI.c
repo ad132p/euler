dafe3bfbd8ce10223a7378286830f6288eba34d1
Arlindo Neto <neto.acs@gmail.com>
2016-12-20 13:03:47 -0200

    Create 2_Fibbo_Sum_lessthan_4MI.c

---
+++ 2_Fibbo_Sum_lessthan_4MI.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#define Four_MI 4000000
+
+unsigned long fibbo_sum () {
+    unsigned long aux[4] = {1, 1, 1, 1};
+    unsigned long my_sum;
+    for (unsigned long i = 0; i < Four_MI; i++) {
+        if (aux[3] % 2 == 0) {
+           my_sum += aux[3]; 
+        }
+        aux[0] =  aux[3];
+        aux[3] += aux[2];
+        aux[1] =  aux[2];
+        aux[2] =  aux[0];
+    }
+    return my_sum;
+}
+
+int main () {
+    unsigned long sum = fibbo_sum();
+    printf("Sum of even number from Fibbonacci sequence until 4 million: %lu", sum);
+    return 0;
+}

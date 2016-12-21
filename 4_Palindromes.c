2784f021185d615b882288f0f0e9ce158111b84f
Arlindo Neto <neto.acs@gmail.com>
2016-12-21 09:18:51 -0200

    Update 4_Palindromes.c

--- 4_Palindromes.c
+++ 4_Palindromes.c
@@ -2,26 +2,30 @@
 
 
 //TODO!!!
-void Pali_loop (int L, int R) {
-    for (int i = 999**2; int > 100**2; i--) {
-        char Int_to_str[7];
-        sprints(Int_to_str, "%d", L * R);
-        if ()
-    }
-}
-
-int right = strlen(str);
 
 // The following function checks whether a string is Palindrome:
 
 int Palindrome(char *str, int left, int right) {
     if (str[left] == str[right]) {
         if (left >= right) {
+            printf("Palindrome found! Largest Palindrome: %s\n", str);
             return 1;
         }
         Palindrome(str, left++, right--);
     }
     return 0;
+}
+
+
+void Pali_loop (int L, int R) {
+    for (int i = 999**2; int > 100**2; i--) {
+        char Int_to_str[7];
+        sprints(Int_to_str, "%d", L * R);
+        Palindrome(Int_to_str, 0, strlen(Int_to_str));
+        if (L == R) {
+            
+        }
+    }
 }
 
 int main () {

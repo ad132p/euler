ee0cac004c02c292415f9b412996e2b1bb86c38c
Arlindo Neto <neto.acs@gmail.com>
2016-12-26 08:19:26 -0200

    Update 4_Palindromes.c

--- 4_Palindromes.c
+++ 4_Palindromes.c
@@ -1,38 +1,42 @@
-#include <stdio.h> 
-
+#include <stdio.h>
+#include <string.h>
 
 //TODO!!!
 
 // The following function checks whether a string is Palindrome:
 
 int Palindrome(char *str, int left, int right) {
-    if (str[left] == str[right]) {
-        if (left >= right) {
-            printf("Palindrome found! Largest Palindrome: %s\n", str);
-            return 1;
+    for (;left <= right; left++, right--) {
+        printf("%d    %d\n", left, right);
+        if (str[left] != str[right - 1]) {
+            printf("You have failed\n");
+            return 0;
         }
-        Palindrome(str, left++, right--);
     }
-    return 0;
+    printf("You have succeeded!!!\n");
+    return 1;
 }
 
-
-void Pali_loop (int L, int R) {
-    for (int i = 999**2; int > 100**2; i--) {
-        char Int_to_str[7];
-        sprints(Int_to_str, "%d", L * R);
-        Palindrome(Int_to_str, 0, strlen(Int_to_str));
-        if (L == R) {
-            
+void Pali_loop(int *p_left, int *p_right) {
+    for (int i = 100; i < *p_right; *p_right--) {
+        int begin = 0;
+        char product[7];
+        sprintf(product, "%d", *p_left * *p_right);
+        int end = strlen(product);
+        if (Palindrome(product, begin, end - 1)) {
+            printf("Multipliers:%d, %d\n", *p_left, *p_right);
+            printf("Product:%d\n", *p_left * *p_right);
         }
     }
 }
 
 int main () {
-
-    int L, R;
-    L = 999;
-    R = L;
-    Pali_loop(L, R);
-
+    int left, right;
+    int *p_left, *p_right;
+    p_left = &left;
+    p_right = &right;
+    left = 999;
+    right = left;
+    Pali_loop(p_left, p_right);
+    return 0;
 }

28c8ab20db29d253d8e13e630a6a0db8911abf4f
Arlindo Neto <neto.acs@gmail.com>
2016-12-30 12:33:40 -0200

    Update 4_Palindromes.c

--- 4_Palindromes.c
+++ 4_Palindromes.c
@@ -9,7 +9,6 @@ int Palindrome(char *str, int left, int right) {
     for (;left <= right; left++, right--) {
         printf("%d    %d\n", left, right);
         if (str[left] != str[right - 1]) {
-            printf("You have failed\n");
             return 0;
         }
     }
@@ -17,24 +16,23 @@ int Palindrome(char *str, int left, int right) {
     return 1;
 }
 
-void Pali_loop(int *p_left, int *p_right) {
-    for (int i = 100; i < *p_right; *p_right--) {
-        int begin = 0;
-        char product[7];
-        sprintf(product, "%d", *p_left * *p_right);
-        int end = strlen(product);
-        if (Palindrome(product, begin, end - 1)) {
-            printf("Multipliers:%d, %d\n", *p_left, *p_right);
-            printf("Product:%d\n", *p_left * *p_right);
+void Pali_loop(int left, int right) {
+    for (int i = 100; i < right; right--) {
+        for (int j = 100; j < left; left--) {
+            int begin = 0;
+            char product[7];
+            sprintf(product, "%d", left * right);
+            int end = strlen(product);
+            if (Palindrome(product, begin, end - 1)) {
+                printf("Multipliers:%d, %d\n", left, right);
+                printf("Product:%d\n", left * right);
+            }
         }
     }
 }
 
 int main () {
     int left, right;
-    int *p_left, *p_right;
-    p_left = &left;
-    p_right = &right;
     left = 999;
     right = left;
     Pali_loop(p_left, p_right);

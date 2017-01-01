f65d3871502af325e55df0645f5cc60e8cb6d428
Arlindo Neto <neto.acs@gmail.com>
2016-12-31 22:22:58 -0200

    Add files via upload

---
+++ Palindrome.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//TODO!!!
+
+// The following function checks whether a string is Palindrome:
+
+int Palindrome(char *str, int left, int right) {
+//  printf("Indexes:%d, %d\n", left, right);
+    for (;left <= right; left++, right--) {
+        if (str[left] != str[right]) {
+            return 0;
+        }
+    }
+//  printf("You have succeeded!!!\n");
+    return 1;
+}
+
+void Pali_loop(char *result) {
+    for (int left = 100; left < 999; left++) {
+        for (int right = 100; right < 999; right++) {
+            int begin = 0;
+            char product[7];
+            sprintf(product, "%d", left * right);
+            int end = strlen(product);
+            if (Palindrome(product, begin, end - 1)) {
+//              printf("Multipliers:%d, %d\n", left, right);
+                printf("Product:%d\n", left * right);
+                if (left * right > atoi(result)) {
+                    strcpy(result, product);
+                }
+            }
+        }
+    }
+    return;
+}
+
+int main () {
+    char result[7];
+    Pali_loop(result);
+    printf("Largest Palindrome: %s\n", result);
+    return 0;
+
+}

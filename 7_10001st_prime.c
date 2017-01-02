a2492e7773af11af052a2e5950f42fa6f0840da1
Arlindo Neto <neto.acs@gmail.com>
2017-01-02 00:49:36 -0200

    Add files via upload

---
+++ 7_10001st_prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int is_it_prime(int number) {
+    for (int j = 2; j < number; j++) {
+        if (number % j == 0) {
+            return false;
+       }
+    }
+    return true;
+}
+
+int prime_counter() {
+    int i = 0;
+    int counter = 0;
+    while (counter < 10001) {
+        ++i;
+        if (is_it_prime(i)) {
+            ++counter;
+        }
+    }
+    return i;
+}
+
+
+
+int main () {
+    printf("10001st prime is: %d\n", prime_counter());
+    return 0;
+}
